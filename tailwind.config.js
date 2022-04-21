202f58854ad432f7babfcdd156f8057aed057b92
andytrj95 <andy.aziez@gmail.com>
2022-04-21 15:09:57 +0700

    tes rong

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        warnadasar: "#10b981",
+        warnatext: "#0f172a",
+        warnatext2: "#64748b",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
